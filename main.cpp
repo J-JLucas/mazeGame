24c054927331a0548fa4a2b4f627f36143b2381f
JoSh <jlucas205@proton.me>
2025-03-09 14:36:21 -0600

    removed old triangle stuff

--- main.cpp
+++ main.cpp
@@ -8,27 +8,7 @@ int main()
     return -1;
   }
 
-  // Define our triangle geometry
-  // vertices are defined in Normalized Device Coordinates (NDC)
-  // NDC is a coordinate system where the visible range is from -1 to 1,
-  // centered at 0,0 OpenGL will transform these coordinates to screen
-  // coordinates OpenGL expects vertices to be defined in counter-clockwise
-  // order OpenGL uses right-handed coordinate system
-  float triangle_verts[] = {
-      -0.5f, -0.5f, 0.0f, // bottom left
-      0.5f,  -0.5f, 0.0f, // bottom right
-      0.0f,  0.5f,  0.0f  // top
-  };
-
-  float triangle_colors[] = {
-      1.0f, 0.0f, 0.0f, // red
-      0.0f, 1.0f, 0.0f, // green
-      0.0f, 0.0f, 1.0f  // blue
-  };
-
   /* SHADERS */
-  // Shaders are written in GLSL (OpenGL Shading Language)
-
   // Load shader source files
   std::string vertexShaderSource = loadShaderSource("../shaders/basic.vert");
   std::string fragmentShaderSource = loadShaderSource("../shaders/basic.frag");
@@ -46,7 +26,6 @@ int main()
   glCompileShader(fragmentShader);
 
   // Shader Program
-  // A shader program is a collection of shaders that are linked together
   unsigned int shaderProgram = glCreateProgram();
 
   // link compiled shaders to shader program
@@ -56,59 +35,39 @@ int main()
 
   // tell OpenGL to use our shader program
   glUseProgram(shaderProgram);
-
-  // doesn't really delete them,
-  // just marks them for deletion once the shaderprogram is finished with them
   glDeleteShader(vertexShader);
   glDeleteShader(fragmentShader);
 
   // 1)
   // Vertex Array Object (VAO)
-  // used to hold active vertix attribute configurations supplied by VBO's
   unsigned int VAO;
   glGenVertexArrays(1, &VAO);
   glBindVertexArray(VAO);
 
   // 2)
   // Create Vertex Buffer Object (VBO)
-  // VBO is a buffer in GPU memory that stores vertex data
-  // VBOs are used to store vertex attributes (position, color, normal, etc)
-  // different data types requires different VBO macros
-  // The more data you send at once, the better the performance
   unsigned int VBO_points;
   glGenBuffers(1, &VBO_points);
-
   // bind VBO buffer to GL_ARRAY_BUFFER target (vertex array type)
   glBindBuffer(GL_ARRAY_BUFFER, VBO_points);
 
   // copy vertex data to bound GL_ARRAY_BUFFER (our VBO buffer)
-  // 3 draw modes:
-  // GL_STATIC_DRAW: the data is set once and used many times; static objects
-  // GL_DYNAMIC_DRAW: the data will be changed a lot and used often; moving
-  // objects GL_STREAM_DRAW: the data is set only once and only used a few
-  // times; usecase? idk
-  glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_verts), triangle_verts,
-               GL_STATIC_DRAW);
+  // glBufferData(GL_ARRAY_BUFFER, sizeof(object_vertices), object_vertices,
+  // GL_STATIC_DRAW);
 
   // 3)
   // tell OpenGL how to interpret vertex data
-  // (this can be done in the render loop for dynamic changes)
-  // 0: attribute location (defined in the vertex shader)
-  // 3: number of components per vertex attribute ie. vec3 = 3
-  // GL_FLOAT: data type
-  // GL_FALSE: normalize data?
-  // 3 * sizeof(float): stride (space between consecutive vertex attributes)
-  // (void*)0: offset of the first component
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0); // tell shader to turn on location 0 attribute
 
   unsigned int VBO_colors;
   glGenBuffers(1, &VBO_colors);
   glBindBuffer(GL_ARRAY_BUFFER, VBO_colors);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_colors), triangle_colors,
-               GL_STATIC_DRAW);
+  // glBufferData(GL_ARRAY_BUFFER, sizeof(object_rbg), object_rgb,
+  //              GL_STATIC_DRAW);
   glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(1); // tell shader to turn on location 1 attribute
+  // glEnableVertexAttribArray(1); // tell shader to turn on location 1
+  // attribute
 
   // Main Render Loop
   while (!glfwWindowShouldClose(window)) {
@@ -119,11 +78,11 @@ int main()
     }
 
     // Clear the screen
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     // draw to double buffer
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glDrawArrays(GL_TRIANGLES, 0, 3);
     // glDrawArrays(GL_LINE_LOOP, 0, 3);
 
     // check for events and swap buffers, rendering triangle to screen
